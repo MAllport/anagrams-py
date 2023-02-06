92cf43b94049ac21e6a5ae11c150e82f212f0337
MAllport <mmallport@gmail.com>
2023-02-06 15:29:26 +0100

    Added documentation and more type hints

--- src/anagrams.py
+++ src/anagrams.py
@@ -1,24 +1,53 @@
 from collections import defaultdict, Counter
+from typing import List, DefaultDict
+
 
 class Anagram:
-    def __init__(self, test_caller):
-        self.test_caller = test_caller
+    """The class holds all algorithmic implementations of solving the
+    anagram problem, as well as helper methods
+    """
+    def __init__(self, stdout: bool):
+        """Determines whether algorithms will write to standard output. 
+        Should be "false" during tests.
+
+        Args:
+            stdout (bool): Writes results to standard output if "true"
+        """
+        self.stdout = stdout
     
     @staticmethod
-    def nor_ord(c: chr):
+    def nor_ord(c: str) -> int:
+        """Finds the ordinal, 0-indexed value of a given character
+        in the Norwegian alphabet
+
+        Args:
+            c (str): Character to find the ordinality of
+
+        Returns:
+            int: Ordinal value of "c"
+        """
         match ord(c):
             case 230: # Æ
-                alphabet_chr = 26
+                alphabet_ordinal = 26
             case 248: # Ø
-                alphabet_chr = 27
+                alphabet_ordinal = 27
             case 229: # Å
-                alphabet_chr = 28
+                alphabet_ordinal = 28
             case _:
-                alphabet_chr = ord(c) - ord('a')
-        return alphabet_chr
+                alphabet_ordinal = ord(c) - ord('a')
+        return alphabet_ordinal
 
-    def naive_anagram(self, words: list[str]):
-        anagrams = defaultdict(list)
+    def sorted_anagram(self, words: List[str]) -> List[List[str]]:
+        """Finds and groups anagrams from a list of words. Uses the fact that
+        two words are anagrams when their sorted
+
+        Args:
+            words (List[str]): Input list of words
+
+        Returns:
+            List[List[str]]: List of grouped anagrams
+        """
+        anagrams: DefaultDict[str, list] = defaultdict(list)
         for word in words:
             s_word = "".join(sorted(word.lower()))
             
@@ -27,13 +56,22 @@ class Anagram:
         
         result = [anagrams for anagrams in list(anagrams.values())
                 if len(anagrams) > 1]
-        if not self.test_caller:
+        if self.stdout:
             for s in result:
                 print(" ".join(s))
         return result
     
-    def count_anagram(self, words: list[str]):
-        anagrams = defaultdict(list)
+    def count_anagram(self, words: List[str]) -> List[List[str]]:
+        """Finds and groups anagrams from a list of words. Uses the fact that
+        two words with the same character counts are anagrams
+
+        Args:
+            words (List[str]): Input list of words
+
+        Returns:
+            List[List[str]]: List of grouped anagrams
+        """
+        anagrams: DefaultDict[tuple, list] = defaultdict(list)
         for word in words:
             counts = [0] * 29 # Norwegian alphabet
             for c in word.lower():
@@ -44,31 +82,51 @@ class Anagram:
         
         result = [anagrams for anagrams in list(anagrams.values())
                 if len(anagrams) > 1]
-        if not self.test_caller:
+        if self.stdout:
             for s in result:
                 print(" ".join(s))
         return result
     
-    def count_anagram_improved(self, words: list[str]):
-        anagrams = defaultdict(list)
+    def count_anagram_improved(self, words: List[str]) -> List[List[str]]:
+        """Finds and groups anagrams from a list of words. Uses the fact that
+        two words with the same character counts are anagrams.
+        In contrast to "count_anagram()", this method uses the "Counter"
+        object to improve performance.
+
+        Args:
+            words (List[str]): Input list of words
+
+        Returns:
+            List[List[str]]: List of grouped anagrams
+        """
+        anagrams: DefaultDict[tuple, list] = defaultdict(list)
         for word in words:
-            key = tuple(Counter(word))
+            key = tuple(Counter(word.lower()))
             if word not in anagrams[key]:
                 anagrams[key].append(word)
         
         result = [anagrams for anagrams in list(anagrams.values())
                 if len(anagrams) > 1]
-        if not self.test_caller:
+        if self.stdout:
             for s in result:
                 print(" ".join(s))
         return result
 
-def read_file_to_list(file_path):
+def read_file_to_list(file_path: str) -> List[str]:
+    """Reads a text file into a list, with newlines as a separator
+    between list elements
+
+    Args:
+        file_path (str): File path to read
+
+    Returns:
+        List[str]: List of file contents
+    """
     with open(file_path, 'r') as data_file:
         words = [line.rstrip() for line in data_file]
     return words
 
 if __name__ == "__main__":
-    file_path = '../datasets/100x_chars_population_100000.txt'
-    a = Anagram(test_caller = False)
-    a.naive_anagram(read_file_to_list(file_path))
+    file_path = '../datasets/population.txt'
+    a = Anagram(stdout = True)
+    a.count_anagram_improved(read_file_to_list(file_path))
--- test/tests.py
+++ test/tests.py
@@ -3,54 +3,105 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 from anagrams import Anagram, read_file_to_list
+from typing import List
 
-anagram = Anagram(test_caller= True)
-#target = anagram.naive_anagram
+anagram = Anagram(stdout= False)
+#target = anagram.sorted_anagram
 #target = anagram.count_anagram
 target = anagram.count_anagram_improved
 
 class TestAnagram(unittest.TestCase):
-    def assert_equal_unordered_2d_list(self, results, solutions):
+    """Class that holds tests for the implementations
+
+    Args:
+        unittest (_type_): "unittest" library base class
+    """
+    def assert_equal_unordered_2d_list(self, results: List[List[str]],
+                                       solutions: List[List[str]]) \
+                                           -> None:
+        print(solutions)
         print(results)
+        """Asserts whether two 2D lists are equal when both the outer and inner
+        elements are not in the same order. This is because the anagram 
+        implementations do not guarantee any specific ordering of anagrams.
+        
+        If the result:
+        1) is the same length as the solution (same number of anagram groups)
+        2) encompasses at least the same anagram groups as the solution
+        ..we claim they are logically equal
+        
+        Example:
+        [["Line", "Elin"], ["Erik", "Lire"]] should be equal to
+        [["Lire", "Erik"], ["Elin", "Line"]]
+        
+        Downside - the assertTrue statement does not give accurate information
+        to the developer whether the algorithm is partially correct but
+        incomplete, or is forming the wrong anagrams.
+
+        Args:
+            results (List[List[str]]): Result from anagram implementation
+            solutions (List[List[str]]): Solution to the problem
+        """
         self.assertEqual(len(results), len(solutions))
         for solution in solutions:
             solution_in_results = any(sorted(solution) == sorted(r)
                                       for r in results)
             self.assertTrue(solution_in_results)
     
-    def runner(self, filepath, solutions):
+    def runner(self, filepath: str, solutions: List[List[str]]) -> None:
+        """Runs the tests given a file path to a dataset and the solution
+
+        Args:
+            filepath (str): File path to a dataset
+            solutions (List[List[str]]): Anagram solutions to the dataset
+        """
         data = read_file_to_list(filepath)
         result = target(data)
         self.assert_equal_unordered_2d_list(result,solutions)
+    
+    """--- START OF TESTS ---"""
         
-    def test_empty_input(self):
+    def test_empty_input(self) -> None:
+        """Tests for correct result when given an empty file
+        """
         self.runner(filepath="../datasets/empty_input.txt", 
                solutions=[])
     
-    def test_only_lowercase(self):
+    def test_only_lowercase(self) -> None:
+        """Tests for correct result when all words are lowercase
+        """
         self.runner(filepath="../datasets/population_lowercase.txt", 
                solutions=[['elin','line']])
     
-    def test_no_anagram(self):
+    def test_no_anagram(self) -> None:
+        """Tests for correct result when there are no anagrams
+        """
         self.runner(filepath="../datasets/population_no_anagram.txt", 
                solutions=[])
     
-    def test_only_duplicates(self):
+    def test_only_duplicates(self) -> None:
+        """Tests for correct result when there only exists duplicates
+        """
         self.runner(filepath="../datasets/population_only_duplicates.txt", 
                solutions=[])
     
-    def test_multiple_groups(self):
+    def test_multiple_groups(self) -> None:
+        """Tests for the correct result when there
+        is an expanded dataset with more than one anagram group
+        """
         self.runner(filepath="../datasets/population_more_groups.txt", 
                solutions=[["Kire", "Erik"], ["Elin", "Line"], 
                           ["Stian", "Nista"]])
     
-    def test_basecase(self):
+    def test_basecase(self) -> None:
+        """Tests the given population.txt input
+        """
         self.runner(filepath="../datasets/population.txt", 
                solutions=[['Elin','Line']])
     
-    def test_basecase_long(self):
-        self.runner(filepath="../datasets/100x_chars_population_100000.txt", 
-               solutions=[['Elin','Line']])
+    #def test_basecase_long(self):
+    #    self.runner(filepath="../datasets/100x_chars_population_100000.txt", 
+    #           solutions=[['Elin','Line']])
     
 if __name__ == 'main':
     unittest.main()
